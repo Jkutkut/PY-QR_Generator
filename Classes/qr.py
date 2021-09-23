daf038c4fd514c8eaae33471ace914b812aae360
jkutkut <jorgeregonzalez@gmail.com>
2021-09-23 10:55:09 +0200

    Corners refactored

--- Classes/qr.py
+++ Classes/qr.py
@@ -1,3 +1,4 @@
+from typing import Generator
 from Classes.colors import Color;
 from PIL import Image, ImageDraw
 
@@ -13,27 +14,36 @@ class QR:
         self.SIZE = 28
         self.SIZE = (self.SIZE, self.SIZE)
 
-        # with Image.open("hopper.jpg") as im:
         with Image.new('RGB', self.SIZE, color=(29, 184, 245)) as im:
 
             self.draw = ImageDraw.Draw(im)
+
+            # Debug cross
+            self.draw.line((0, 0) + im.size, fill=128)
+            self.draw.line((0, im.size[1], im.size[0], 0), fill=128)
 
             # Corners
             self.drawCorner(0, 0)
             self.drawCorner(22, 0)
             self.drawCorner(0, 22)
 
-            # Debug cross
-            self.draw.line((0, 0) + im.size, fill=128)
-            self.draw.line((0, im.size[1], im.size[0], 0), fill=128)
+            #  Timing
 
             # write to stdout
             im.save("qr.png")
 
     def drawCorner(self, x: int, y: int):
         w = 1
-        rect = [(0 + i, 0 + i, 6 - i, 6 - i) for i in range(3)]
+        rect = [(-1 + i, -1 + i, 7 - i, 7 - i) for i in range(4)]
         rect = [((c[0] + x) * w, (c[1] + y) * w, (c[2] + x) * w, (c[3] + y) * w) for c in rect]
-        self.draw.rectangle(rect[0], fill=Color.QR_COLOR)
-        self.draw.rectangle(rect[1], fill=Color.BG)
-        self.draw.rectangle(rect[2], fill=Color.QR_COLOR)
+
+        color = QR.blinkColor()
+
+        for r in rect:
+            self.draw.rectangle(r, next(color))
+
+
+    def blinkColor() -> Generator[tuple, None, None]:
+        while(True):
+            yield Color.BG
+            yield Color.QR_COLOR
