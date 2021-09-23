1f2d11f4ba75c0d7cc1319f1ef4cd5ba7fe4a69e
jkutkut <jorgeregonzalez@gmail.com>
2021-09-23 13:32:31 +0200

    positioning method added

--- Classes/qr.py
+++ Classes/qr.py
@@ -33,16 +33,29 @@ class QR:
                 self.drawPixel(i, 6, c)
                 self.drawPixel(6, i, c)
 
+            # Alignment
+            self.drawPositioning(22, 22)
+
             # write to stdout
             im.save("qr.png")
 
-    def drawPixel(self, x: int, y: int, color: tuple) -> None:
+    def drawPixel(self, x: int, y: int, color: tuple = Color.QR_COLOR) -> None:
         '''Draws a 1x1 rectangle on the given location.'''
         self.drawRect((x, y, x, y), color)
 
     def drawRect(self, coord: tuple, color: tuple) -> None:
         '''Draws a rectangle on the given coordinate (xi, yi, xf, yf) with the given RGB color.'''
         self.draw.rectangle(coord, color)
+
+    
+    # Positioning and orientation
+    def drawPositioning(self, x: int, y: int):
+        rect = [(-2 + i, -2 + i, 2 - i, 2 - i) for i in range(3)]
+        rect = [(c[0] + x, c[1] + y, c[2] + x, c[3] + y) for c in rect]
+
+        color = QR.blinkColor(firstBG=False)
+        for r in rect:
+            self.drawRect(r, next(color))
 
     def drawCorner(self, x: int, y: int):
         '''Draws a corner of the QR on the given coordinates.'''
