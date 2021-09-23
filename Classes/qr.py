a9fd4db9ba35d4c5d4ac935247c04c42e3edd8a1
jkutkut <jorgeregonzalez@gmail.com>
2021-09-23 10:36:17 +0200

    Code now developed on qr class

--- Classes/qr.py
+++ Classes/qr.py
@@ -1,5 +1,39 @@
+from Classes.colors import Color;
+from PIL import Image, ImageDraw
+
+
 class QR:
     '''Class responsible of generating QR codes.'''
     
     def __init__(self, text: str):
         print("hello world!")
+
+        self.draw: ImageDraw
+
+        self.SIZE = 28
+        self.SIZE = (self.SIZE, self.SIZE)
+
+        # with Image.open("hopper.jpg") as im:
+        with Image.new('RGB', self.SIZE, color=(29, 184, 245)) as im:
+
+            self.draw = ImageDraw.Draw(im)
+
+            # Corners
+            self.drawCorner(0, 0)
+            self.drawCorner(22, 0)
+            self.drawCorner(0, 22)
+
+            # Debug cross
+            self.draw.line((0, 0) + im.size, fill=128)
+            self.draw.line((0, im.size[1], im.size[0], 0), fill=128)
+
+            # write to stdout
+            im.save("qr.png")
+
+    def drawCorner(self, x: int, y: int):
+        w = 1
+        rect = [(0 + i, 0 + i, 6 - i, 6 - i) for i in range(3)]
+        rect = [((c[0] + x) * w, (c[1] + y) * w, (c[2] + x) * w, (c[3] + y) * w) for c in rect]
+        self.draw.rectangle(rect[0], fill=Color.QR_COLOR)
+        self.draw.rectangle(rect[1], fill=Color.BG)
+        self.draw.rectangle(rect[2], fill=Color.QR_COLOR)
--- main.py
+++ main.py
@@ -1,43 +1,9 @@
 #!/usr/bin/env python3
 
-from PIL import Image, ImageDraw
+from Classes.qr import QR
 
-# Constants
-BG = (255, 255, 255)
-QR_COLOR = (0, 0, 0)
-
-SIZE = 280
-SIZE = (SIZE, SIZE)
-
-def drawCorner(x, y):
-    w = 10
-    rect = [(0 + i, 0 + i, 6 - i, 6 - i) for i in range(3)]
-    rect = [((c[0] + x) * w, (c[1] + y) * w, (c[2] + x) * w, (c[3] + y) * w) for c in rect]
-    draw.rectangle(rect[0], fill=QR_COLOR)
-    draw.rectangle(rect[1], fill=BG)
-    draw.rectangle(rect[2], fill=QR_COLOR)
-
-# with Image.open("hopper.jpg") as im:
-with Image.new('RGB', SIZE, color=(29, 184, 245)) as im:
-
-    draw = ImageDraw.Draw(im)
-
-	# Corners
-    drawCorner(0, 0)
-    drawCorner(22, 0)
-    drawCorner(0, 22)
-    # draw.rectangle((0, 0, 6, 6), fill=QR_COLOR)
-    # draw.rectangle((1, 1, 5, 5), fill=BG)
-    # draw.rectangle((2, 2, 4, 4), fill=QR_COLOR)
-
-
-
-    # Debug cross
-    draw.line((0, 0) + im.size, fill=128)
-    draw.line((0, im.size[1], im.size[0], 0), fill=128)
-
-    # write to stdout
-    im.save("qr.png")
-
-print("Thanks for using this code, I hope you liked it.")
-print("See more projects like this one on https://github.com/jkutkut/")
+if __name__ == "__main__":
+    qrCode = QR("hello word")
+    
+    print("Thanks for using this code, I hope you liked it.")
+    print("See more projects like this one on https://github.com/jkutkut/")
