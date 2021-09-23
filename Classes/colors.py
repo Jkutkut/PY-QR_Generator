7ab5961ab9472ca564dc5c5b4232cf4a9df5b9cc
jkutkut <jorgeregonzalez@gmail.com>
2021-09-23 08:09:26 +0200

    Color class now using dataclass decorator

--- Classes/colors.py
+++ Classes/colors.py
@@ -1,18 +1,7 @@
-def constant(f): #define of a constant class
-	def fset(self, value):
-		raise TypeError
-	def fget(self):
-		return f()
-	return property(fget, fset)
+from dataclasses import dataclass;
 
-
-class color(object): #Create the COLOR class as a collection of constants
-	@constant
-	def BG(): # background
-		return (25, 25, 25) 
-	@constant
-	def GREY(): # Color of the grid
-		return (128, 128, 128)
-	@constant
-	def WHITE(): # Color of a living cell
-		return (255, 255, 255)
+@dataclass
+class Color:
+	'''Class with the colors used on the python code.'''
+	BG: tuple = (255, 255, 255)
+	QR_COLOR: tuple = (0, 0, 0)
