61c588c236d85847c710f408f53f6479a06283b5
jkutkut <jorgeregonzalez@gmail.com>
2021-09-24 08:07:24 +0200

    qrcode library will be used to test the logic

--- main.py
+++ main.py
@@ -2,8 +2,24 @@
 
 from Classes.qr import QR
 
+def spectedQR(text):
+    import qrcode
+    qr = qrcode.QRCode(
+        error_correction=qrcode.constants.ERROR_CORRECT_H
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img.save("qr_spected.png")
+
 if __name__ == "__main__":
-    qrCode = QR("hello word")
+    text = 'HELLO WORLD'
+    # qrCode = QR(text)
+    spectedQR(text)
+    
+    
     
     print("Thanks for using this code, I hope you liked it.")
     print("See more projects like this one on https://github.com/jkutkut/")
